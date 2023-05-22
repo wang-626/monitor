630996659f3cb2e8134f0d3a22fada6d6f06a281
david <62385136+wang-626@users.noreply.github.com>
2023-05-22 12:04:16 +0800

    Add fun set_output/set_int put/set_high/set_low

--- repoitory/gpio.py
+++ repoitory/gpio.py
@@ -1,24 +1,35 @@
 '''gpio entity control'''
 
 import sys
-sys.path.append('/usr/lib/python3.9/site-packages/wiringpi-2.60.1-py3.9-linux-aarch64.egg/')
+sys.path.append(
+    '/usr/lib/python3.9/site-packages/wiringpi-2.60.1-py3.9-linux-aarch64.egg/')  # 3.9以上需要
+
 import wiringpi
 from wiringpi import GPIO
-wiringpi.wiringPiSetup();
+
 
 class Gpio():
-    def __init__(self, port):
+    def __init__(self, pin: int) -> None:
         '''Initialize gpio '''
-        self.port = int(port)
-        self.Status = ''
+        wiringpi.wiringPiSetup()
+        self.pin = pin
+        self.mode = 'None'
+        self.status = f'Pin:{str(self.pin)}  Mode:{str(self.mode)}'
 
-    def output(self, value):
-        '''set gpio output'''
-        wiringpi.pinMode(self.port, GPIO.OUTPUT)
-        self.Status = 'Port' + str(self.port) + ' ouput ' +str(value)
+    def set_output(self) -> None:
+        '''set gpio mode output'''
+        wiringpi.pinMode(self.pin, GPIO.OUTPUT)
+        self.mode = 'OUT'
 
-    def input(self):
-        '''set gpio input'''
-        wiringpi.pinMode(self.port, GPIO.IN)
-        self.Status = ' Input ' + str(self.port)
-        return GPIO.input(self.port)
+    def set_input(self) -> None:
+        '''set gpio mode input'''
+        wiringpi.pinMode(self.pin, GPIO.IN)
+        self.mode = 'IN'
+
+    def set_high(self) -> None:
+        '''set gpio output value'''
+        wiringpi.digitalWrite(self.pin, 1)
+
+    def set_low(self) -> None:
+        '''set gpio output value'''
+        wiringpi.digitalWrite(self.pin, 0)

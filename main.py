9d2b5b7c004e5bda483b81d884237823d14b5d4b
david <62385136+wang-626@users.noreply.github.com>
2023-05-26 11:31:56 +0800

    Update init value

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
+import signal
 from apscheduler.schedulers.blocking import BlockingScheduler
 from controller.check_connection import machine_connection
 from repoitory.uart import Uart
-import signal
+from controller.gpio_connectory import GpioConnector
+from repoitory.sql import Database
+from repoitory.gpio import Gpio
+from service.check_network import check_network
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+
+remote_db = {
+    "host": config['db_host'],
+    "port": 3306,
+    "user": config['db_user'],
+    "password": config['db_password'],
+    "database": config['db_name']
+}
+
+
+def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    PORT = "/dev/ttyS5"
+    db = Database(remote_db)
+    db2 = Database(remote_db)
+    uart = Uart(PORT)
+    gpio = Gpio(2)
+    gpio_connector = GpioConnector(gpio, db)
+    task = BlockingScheduler()
+    task.add_job(func=machine_connection, args=[
+                 uart, db2], trigger='interval', seconds=3)
+    task.add_job(func=gpio_connector.check_db_gpio_set,
+                 trigger='interval', seconds=10)
+
+    print('Monitor runnig...')
+    task.start()
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    PORT = "COM4"
-    task = BlockingScheduler()
-    task.add_job(func=machine_connection, args=[
-                 Uart(PORT)], trigger='interval', seconds=3)
-
-    print('runnig...')
-    task.start()
+    main()

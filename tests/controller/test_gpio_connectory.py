dbc78b91151389bd88de0c4bdcaddc87fb58ad0c
david <62385136+wang-626@users.noreply.github.com>
2023-05-26 17:52:58 +0800

    Update fun name

--- tests/controller/test_gpio_connectory.py
+++ tests/controller/test_gpio_connectory.py
@@ -7,7 +7,7 @@ from controller.gpio_connectory import GpioConnector
 
 class GPIO():
     '''mock gpio'''
-    def execute_query_one(self):
+    def query_one(self):
         '''mock'''
 
     def set_high(self):
@@ -27,10 +27,10 @@ class TestGpioConnector(unittest.TestCase):
         '''test sql return 0 or 1 result'''
         mock_db = Mock(spec=Database)  # 模擬的db
         mock_gpio = Mock(GPIO)  # 模擬的gpio
-        mock_db.execute_query_one.return_value = [0]  # sql回傳0
+        mock_db.query_one.return_value = [0]  # sql回傳0
         gpio = GpioConnector(mock_gpio, mock_db)
         self.assertEqual(gpio.gpio_val, 0)
-        mock_db.execute_query_one.return_value = [1]  # sql回傳1
+        mock_db.query_one.return_value = [1]  # sql回傳1
         gpio = GpioConnector(mock_gpio, mock_db)
         gpio.check_db_gpio_set()
         self.assertEqual(gpio.gpio_val, 1)
